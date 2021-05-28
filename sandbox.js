b232aba102e9ce691ff59c715cf29b8a12df06a4
dinesh4900 <dineshm4900@gmail.com>
2021-05-28 11:47:33 +0530

    compiling typescript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var dinesh = 'dinesh';
+console.log(dinesh);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const dinesh = 'dinesh';
+
+console.log(dinesh);
+
+const inputs = document.querySelectorAll('input')
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input); 
+});
